529013a03d4c68d4162f7aaee96ba33a59b24205
Andrej Orešnik <aoresnik7@gmail.com>
2021-01-01 04:57:43 +0100

    Ability to stop a pommodoro in progress

--- PommodoroTimer/PommodoroTimer.ino
+++ PommodoroTimer/PommodoroTimer.ino
@@ -33,10 +33,47 @@ unsigned long waiting_start;
 #define LCD_WIDTH 16
 #define LCD_HEIGHT 2
 
+#define DEBOUNCE_TIME_MS 50
+
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+class Button {
+  // Switch debounce - based on https://www.arduino.cc/en/Tutorial/BuiltInExamples/Debounce
+  private:
+    int input;
+    long lastChangeTime;
+    int lastState;
+
+  public:
+    Button(int _input) : input(_input) {};
+
+    void init() {
+      lastChangeTime = millis();
+      lastState = digitalRead(input);
+    };
+
+    // Returns true if button state has changed from from to to since last call
+    boolean stateChanged(int from, int to) {
+      if (millis() >= lastChangeTime + DEBOUNCE_TIME_MS) {
+        int newState = digitalRead(input);
+        boolean change = (lastState == from && newState == to);
+        if (lastState != newState) {
+          lastState = newState;
+          lastChangeTime = millis();
+        }
+        return change;
+      }
+      else {
+        // Assume that the HW status is unstable - don't read
+        return false;
+      }
+    }
+};
+
+Button startStopButton(SWITCH_START_STOP);
 
 void setup() {
   // set up the LCD's number of columns and rows:
@@ -47,17 +84,20 @@ void setup() {
   pinMode(SWITCH_RESERVED, INPUT);
   pinMode(SWITCH_START_STOP, INPUT);
 
+  startStopButton.init();
+
   waiting_start = millis();
 }
 
 void loop() {
+  char output[LCD_WIDTH + 1];
+  
   switch (state) {
     case pommodoro:
       digitalWrite(LED_WAITING, LOW);
       digitalWrite(LED_POMMODORO, HIGH);
 
       char s[6]; // For number of seconds
-      char output[LCD_WIDTH + 1];
 
       // Centered print number of seconds remaining in the current pommodoro
       lcd.setCursor(0, 1);
@@ -65,6 +105,7 @@ void loop() {
       secondsRemaining = (pommodoro_end_time-millis()+999) / 1000;
       sprintf(s, "%d sec", secondsRemaining);
       memset(output, ' ', LCD_WIDTH);
+      output[LCD_WIDTH] = '\0';
       int s_len;
       s_len = strlen(s);
       memcpy(output + (LCD_WIDTH-s_len+1)/2, s, s_len);
@@ -82,13 +123,24 @@ void loop() {
         state = waiting;
         waiting_start = millis();
       }
+      if (startStopButton.stateChanged(LOW, HIGH)) {
+        state = waiting;
+        waiting_start = millis();
+      }
       break;
     case waiting:
       // Blink the green LED
       digitalWrite(LED_WAITING, (((millis()-waiting_start) / 1000) % 2 != 0) ? HIGH : LOW);
       digitalWrite(LED_POMMODORO, LOW);
 
-      if (digitalRead(SWITCH_START_STOP)) {
+      memset(output, ' ', LCD_WIDTH);
+      output[LCD_WIDTH] = '\0';
+      lcd.setCursor(0, 0);
+      lcd.print(output);
+      lcd.setCursor(0, 1);
+      lcd.print(output);
+
+      if (startStopButton.stateChanged(LOW, HIGH)) {
         pommodoro_end_time = millis() + POMMODORO_MINUTES * 60L * 1000L;
         state = pommodoro;
       }
